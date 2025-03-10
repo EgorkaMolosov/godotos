8706c08c05a1e6cfd593ef1d5d232f53933949ae
EgorkaMolosov <molodtsov.egor.05@yandex.ru>
2025-03-10 23:20:59 +0300

    dfghj

--- scenes/bats.gd
+++ scenes/bats.gd
@@ -1,40 +1,34 @@
-extends AnimatableBody2D
+extends CharacterBody2D
 
 const SPEED = 99
 @onready var animated_bich = $AnimatedSprite2D
-var dead = false
-var already_dead = false
+@onready var player = get_parent().get_node("player")
+@onready var navigation_agent = $NavigationAgent2D
 
 # Called when the node enter"res://scenes/bats.gd"s the scene tree for the first time.
-
+func _ready():
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta):
-	var player = get_parent().get_node("player")
-	var player_position = player.global_position
-	var enemy_position = global_position
-	var direction = player_position - enemy_position
-	direction = direction.normalized()
-	var motion = direction * SPEED * delta
-	move_and_collide(motion)
-	handle_movement_animation(direction)
+	navigation_agent.target_position = player.global_position
+	velocity = global_position.direction_to(navigation_agent.get_next_path_position()) * SPEED
+	move_and_slide()
+	handle_movement_animation()
 	
 func _shit_process(delta: float) -> void:
 	pass
 
-func toggle_flip_sprite(direction):
-	var player = get_parent().get_node("player")
-	if already_dead == false:
-		if global_position > player.global_position:
-			animated_bich.play("default")
-			animated_bich.flip_h = true
-		else:
-			animated_bich.play("default")
-			animated_bich.flip_h = false
+func toggle_flip_sprite():
+	if global_position > player.global_position:
+		animated_bich.play("default")
+		animated_bich.flip_h = true
+	else:
+		animated_bich.play("default")
+		animated_bich.flip_h = false
 
-func handle_movement_animation(direction):
-	animated_bich.play("default")
-	toggle_flip_sprite(direction)
+func handle_movement_animation():
+	toggle_flip_sprite()
