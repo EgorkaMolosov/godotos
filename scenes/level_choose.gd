89faa482fb26690b612ba3473c8275214a35d2b0
EgorkaMolosov <molodtsov.egor.05@yandex.ru>
2025-03-12 23:18:43 +0300

    dxfghghj

--- scenes/level_choose.gd
+++ scenes/level_choose.gd
@@ -1,17 +1,21 @@
 extends Node2D
 @onready var gui = $gui
 @onready var parallax = $ParallaxBackground
+@onready var leave = $gui/Button_Leave
 var scroll_offset := Vector2.ZERO
 var new_pos = 0
+var new_pos_leave = 8
 var cursor = load('res://ui/Cursors_v2/Light/Arrows/Arrow1.png')
 var hand = load('res://ui/Cursors_v2/Light/Hands/Hand1.png')
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	parallax.set_scroll_offset(GlobalInfo.parallax) # Replace with function body.
+	var new_pos_leave = leave.position.x
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	gui.position = gui.position.lerp(Vector2(new_pos, gui.position.y), delta * 10)
+	leave.position = leave.position.lerp(Vector2(new_pos_leave, leave.position.y), delta * 10)
 
 func _on_button_leave_pressed() -> void:
 	get_tree().quit()
@@ -33,8 +37,10 @@ func _on_button_arrow_left_mouse_exited() -> void:
 
 func _on_button_arrow_left__pressed() -> void:
 	new_pos = gui.position.x - 1280
+	new_pos_leave = leave.position.x + 1280
 func _on_button_right_pressed() -> void:
 	new_pos = gui.position.x + 1280
+	new_pos_leave = leave.position.x - 1280
 
 
 func _on_button_change_level_mouse_entered() -> void:
