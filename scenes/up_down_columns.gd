d2caf2182f89263c5faf180f387a8d69c7e26631
EgorkaMolosov <molodtsov.egor.05@yandex.ru>
2025-03-12 19:04:16 +0300

    пгргроо

--- scenes/up_down_columns.gd
+++ scenes/up_down_columns.gd
@@ -7,11 +7,12 @@ var old_pos1 = null
 var old_pos2 = null
 var old_pos3 = null
 var old_pos4 = null
+
 var new_pos1 = null
 var new_pos2 = null
 var new_pos3 = null
 var new_pos4 = null
-var rng = RandomNumberGenerator.new()
+
 var down1 = false
 var down2 = false
 var down3 = false
@@ -32,23 +33,39 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	column1.position.y = lerp(column1.position.y, new_pos1, delta*3.5)
-	column2.position.y = lerp(column2.position.y, new_pos2, delta*5)
-	column3.position.y = lerp(column3.position.y, new_pos3, delta*4)
-	column4.position.y = lerp(column4.position.y, new_pos4, delta*2.5)
-	if column1.position.y == old_pos1+80:
+	column1.position.y = lerp(column1.position.y, new_pos1, delta*7)
+	column2.position.y = lerp(column2.position.y, new_pos2, delta*10)
+	column3.position.y = lerp(column3.position.y, new_pos3, delta*8)
+	column4.position.y = lerp(column4.position.y, new_pos4, delta*5)
+	if column1.position.y > old_pos1+79 and down1 == false:
+		column1.position.y = new_pos1
 		new_pos1 -= 80
-	if column2.position.y == old_pos2+80:
+		down1 = true
+	if column2.position.y > old_pos2+79 and down2 == false:
+		column2.position.y = new_pos2
 		new_pos2 -= 80
-	if column3.position.y > old_pos3+80:
+		down2 = true
+	if column3.position.y > old_pos3+79 and down3 == false:
+		column3.position.y = new_pos3
 		new_pos3 -= 80
-	if column4.position.y > old_pos4+80:
+		down3 = true
+	if column4.position.y > old_pos4+79 and down4 == false:
+		column4.position.y = new_pos4
 		new_pos4 -= 80
-	if column1.position.y == old_pos1:
+		down4 = true
+	if column1.position.y < old_pos1+1 and down1 == true:
+		column1.position.y = old_pos1
 		new_pos1 += 80
-	if column2.position.y == old_pos2:
+		down1 = false
+	if column2.position.y < old_pos2+1 and down2 == true:
+		column2.position.y = old_pos2
 		new_pos2 += 80
-	if column3.position.y == old_pos3:
+		down2 = false
+	if column3.position.y < old_pos3+1 and down3 == true:
+		column3.position.y = old_pos3
 		new_pos3 += 80
-	if column4.position.y == old_pos4:
+		down3 = false
+	if column4.position.y < old_pos4+1 and down4 == true:
+		column4.position.y = old_pos4
 		new_pos4 += 80
+		down4 = false
