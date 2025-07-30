bf1a2821bad43af5be763de26a9b9f704ac3972b
EgorkaMolosov <molodtsov.egor.05@yandex.ru>
2025-07-30 15:04:35 +0300

    куьщму ершы сщьше

--- scenes/player.gd
+++ scenes/player.gd
@@ -24,7 +24,6 @@ var level_ended = false
 func _ready() -> void:
 	level_file = get_parent().scene_file_path
 	parallax.set_scroll_offset(GlobalInfo.parallax)
-	Bridge.advertisement.connect("interstitial_state_changed", Callable(self, "_on_interstitial_state_changed"))
 	GlobalInfo.score = 0
 
 func _process(delta: float) -> void:
@@ -113,7 +112,7 @@ func _on_pickup_area_entered(area: Area2D):
 			elif level_file == 'res://scenes/level_8.tscn':
 				if time < GlobalInfo.time8:
 					GlobalInfo.time8 = time
-					GlobalInfo.level9_done = true
+					GlobalInfo.level8_done = true
 			get_tree().change_scene_to_file('res://scenes/molodec.tscn')
 			#Bridge.advertisement.show_interstitial()
 	elif area.has_method('_honey'):
@@ -166,14 +165,14 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	if position.y > 720:
 		on_death()
 	else:
-		pass # Replace with function body.
+		pass
 
 func _on_pickup_area_body_entered(body: Node2D) -> void:
 	if body.has_method("_shit_process"):
 		if position.y - body.position.y < -3:
-			body.queue_free()
+			body.die()
 		else:
-			on_death()# Replace with function body.
+			on_death()
 
 func _on_timer_timeout() -> void:
-	time += 0.01 # Replace with function body.
+	time += 0.01
